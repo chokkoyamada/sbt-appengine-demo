03d6757ccea9095d17764876154c33aaa1f2093e
Naoyuki Yamada <yamada@kirishikistudios.com>
2017-09-28 16:05:21 +0900

    update

--- src/main/scala/myapp/DemoServlet.scala
+++ src/main/scala/myapp/DemoServlet.scala
@@ -5,12 +5,14 @@ import javax.servlet.http._
 class DemoServlet extends HttpServlet {
 
   override def doGet(req: HttpServletRequest, resp: HttpServletResponse): Unit = {
-    (1 to 5).foreach(
-      i => Seq("A", "B").foreach(
-        j => println(i+"-"+j)
-      )
-    )
+    val result: Int = {
+      for {
+        k <- List(1, 2, 3, 4, 5)
+        j <- List(2, 3, 4, 5, 6)
+      } yield k + j
+    }.sum
+    
     resp.setContentType("application/json")
-    resp.getWriter.println("{'result': 'ok'}")
+    resp.getWriter.println(s"{'result': '$result'}")
   }
 }
